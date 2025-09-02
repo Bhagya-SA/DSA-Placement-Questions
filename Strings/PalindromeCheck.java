5a3b15844c87c04d30fee643729ba903a41ac98b
Bhagya-SA <bhagyaanjaneya@gmail.com>
2025-09-02 21:41:05 +0530

    Create PalindromeCheck.java

---
+++ Strings/PalindromeCheck.java
@@ -0,0 +1,22 @@
+public class PalindromeCheck {
+    public static boolean isPalindrome(String s){
+        int left = 0, right = s.length() - 1;
+        while(left < right){
+            if(s.charAt(left) != s.charAt(right)) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args){
+        String s = "racecar";
+        System.out.println("Is Palindrome? " + isPalindrome(s));
+    }
+}
+
+/*
+Optimal Approach: Two-pointer check
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
